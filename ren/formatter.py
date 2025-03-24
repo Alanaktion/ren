fa2af3e8ab5aa9532d4c658e181dd4f022f8a9dd
Alan Hardman <alanaktion@gmail.com>
2025-03-23 18:52:06 -0600

    Fix format string handling without gallery-dl

--- ren/formatter.py
+++ ren/formatter.py
@@ -2,16 +2,15 @@ from .logging import log
 
 try:
     import gallery_dl.formatter as formatter
-except ImportError:
-    pass
 
-
-def format_impl(format_str: str, *args, **kwargs: dict[str, str | int]):
-    if formatter is not None:
+    def format_impl(format_str: str, *args, **kwargs: dict[str, str | int]):
         if '{}' in format_str:
             # TODO: handle {} with slices, other operations?
             log.warning('Replacing {} with {fullname} for gallery_dl compat')
             format_str = format_str.replace('{}', '{fullname}')
         fmt: formatter.StringFormatter = formatter.parse(format_str)
         return fmt.format_map(kwargs)
-    return format_str.format(*args, **kwargs)
+
+except ImportError:
+    def format_impl(format_str: str, *args, **kwargs: dict[str, str | int]):
+        return format_str.format(*args, **kwargs)
